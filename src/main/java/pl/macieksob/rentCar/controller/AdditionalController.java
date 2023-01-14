ba8780675c94456ffc4aeb5998f01618b633629f
sobek123 <65244789+sobek123@users.noreply.github.com>
2023-01-14 22:53:02 +0100

    Update AdditionalController.java

--- src/main/java/pl/macieksob/rentCar/controller/AdditionalController.java
+++ src/main/java/pl/macieksob/rentCar/controller/AdditionalController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/additional")
-@CrossOrigin("localhost:3000")
+@CrossOrigin("https://sobrent.herokuapp.com")
 public class AdditionalController {
 
     @Autowired
