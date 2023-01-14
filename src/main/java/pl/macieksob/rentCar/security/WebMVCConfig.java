e5bec10e2819c30f31f9692bae4a9ed2aba1f60b
sobek123 <65244789+sobek123@users.noreply.github.com>
2023-01-14 23:22:30 +0100

    Update WebMVCConfig.java

--- src/main/java/pl/macieksob/rentCar/security/WebMVCConfig.java
+++ src/main/java/pl/macieksob/rentCar/security/WebMVCConfig.java
@@ -11,7 +11,7 @@ public class WebMVCConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedMethods("*")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("https://sobrent.herokuapp.com")
                 .allowedHeaders("*")
                 .allowCredentials(false)
                 .maxAge(-1);
